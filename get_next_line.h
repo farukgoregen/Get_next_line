70a2436e45e9beee2574a8212fd3593aa2d6b785
omgorege <147288299+farukgoregen@users.noreply.github.com>
2024-11-03 17:08:44 +0300

    Add files via upload

---
+++ get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omgorege <omgorege@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 16:20:05 by omgorege          #+#    #+#             */
+/*   Updated: 2024/11/03 14:04:17 by omgorege         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 4
+
+# endif
+
+int		ft_strlen(char const *s);
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_next_line(int fd);
+char	*ft_strchr(char *s, int c);
+char	*line_str(char *str);
+char	*ft_new(char *str);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: omgorege <omgorege@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 12:32:58 by omgorege          #+#    #+#             */
+/*   Updated: 2024/11/03 14:51:07 by omgorege         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	ft_strlen(char const *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *str, char *buffer)
+{
+	size_t	c;
+	size_t	i;
+	char	*res;
+
+	if (!str)
+	{
+		str = malloc(1);
+		str[0] = '\0';
+	}
+	c = 0;
+	i = 0;
+	res = malloc(ft_strlen(str) + ft_strlen(buffer) + 1);
+	if (res == NULL)
+		return (NULL);
+	while (str[c])
+	{
+		res[c] = str[c];
+		c++;
+	}
+	while (buffer[i])
+		res[c++] = buffer[i++];
+	res[ft_strlen(str) + ft_strlen(buffer)] = '\0';
+	free(str);
+	return (res);
+}
+
+char	*ft_strchr(char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] != '\0')
+	{
+		if (str[i] == (char)c)
+			return ((char *)&str[i]);
+		i++;
+	}
+	return (0);
+}
+
+char	*line_str(char *str)
+{
+	size_t	i;
+	char	*ptr;
+
+	i = 0;
+	if (!str[i])
+		return (NULL);
+	while (str[i] && str[i] != '\n')
+		i++;
+	ptr = malloc(i + 2);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_new(char *str)
+{
+	char	*res;
+	int		i;
+	int		j;
+
+	j = 0;
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!str[i])
+	{
+		free(str);
+		return (NULL);
+	}
+	res = malloc((ft_strlen(str) - i) + 1);
+	if (!res)
+		return (NULL);
+	i++;
+	while (str[i])
+		res[j++] = str[i++];
+	res[j] = '\0';
+	free(str);
+	return (res);
+}
